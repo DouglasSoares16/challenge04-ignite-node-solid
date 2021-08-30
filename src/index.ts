b6d82f5608dbc7d1a5458c9536834195657c271d
Douglas Soares <douglasmaismcpe@gmail.com>
2021-08-30 12:14:50 -0300

    project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 import { usersRoutes } from "./routes/users.routes";
 
@@ -7,5 +7,18 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({ error: err.message });
+    }
+
+    return response.status(500).json({
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
+);
 
 export { app };
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,15 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    const user = this.showUserProfileUseCase.execute({ user_id });
+
+    if (!user) {
+      throw new Error("User does not exists!");
+    }
+
+    return response.json(user);
   }
 }
 
